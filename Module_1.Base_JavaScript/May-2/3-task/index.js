8162f972c7947684dc9f2a658533d7c15b6e62cf
Alexandermor2 <saha353033@ro.ru>
2022-11-07 16:11:02 +0300

    1.Hello World

--- Module_1.Base_JavaScript/May-2/3-task/index.js
+++ Module_1.Base_JavaScript/May-2/3-task/index.js
@@ -1,3 +1,30 @@
-/**
- * Учим хрючим88888888
- */
+//1.string
+const favoriteDrink ='Beer';
+console.log( favoriteDrink);
+
+//2.number
+const numberofCups = 2;
+console.log(numberofCups);
+
+//3. boolean
+const isColdDrink = true;
+console.log(isColdDrink);
+
+//5. null - пусто, значение не известно
+const studentFavoriteDrink = null;
+console.log(studentFavoriteDrink);
+
+//5. undefined - значение не было присвоено
+let carOwner;
+console.log(carOwner);
+
+//6. object
+const drink = {
+    favoriteDrink: 'Beer',
+    numberofCups: 2,
+    isColdDrink: true,
+};
+console.log(drink);
+
+//7. symbol
+
